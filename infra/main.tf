ad837e4f10a19f65a0827cc6eef3e6c97dbf0ee3
JesseSBezerra <jessebezerra@hotmail.com.br>
2025-03-17 09:16:41 -0300

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -17,14 +17,17 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+data "aws_vpc" "main" {
+  filter {
+    name   = "cidr-block"
+    values = ["10.0.0.0/16"]
+  }
 }
 
 resource "aws_subnet" "public" {
   count                   = 2
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
+  vpc_id                  = data.aws_vpc.main.id
+  cidr_block              = cidrsubnet(data.aws_vpc.main.cidr_block, 8, count.index)
   map_public_ip_on_launch = true
   availability_zone       = data.aws_availability_zones.available.names[count.index]
 }
