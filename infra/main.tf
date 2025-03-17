45091fd64a5ab3f051878e23c2d54a5c939664b2
JesseSBezerra <jessebezerra@hotmail.com.br>
2025-03-17 09:29:27 -0300

    ajuste para subir segunda aplicao

--- infra/main.tf
+++ infra/main.tf
@@ -22,6 +22,11 @@ data "aws_vpc" "main" {
     name   = "cidr-block"
     values = ["10.0.0.0/16"]
   }
+
+  filter {
+    name   = "tag:Name"
+    values = ["hub-lancamentos-vpc"]
+  }
 }
 
 resource "aws_subnet" "public" {
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,6 +1,6 @@
 variable "region" { default = "sa-east-1" }
 variable "app_name" { default = "hub-emprestimos" }
 variable "ecr_image_url" { default = "397685870114.dkr.ecr.sa-east-1.amazonaws.com/hub-emprestimos:latest" }
-variable "container_port" { default = 8080 }
+variable "container_port" { default = 8011 }
 variable "ecr_repo_name" { default = "hub-emprestimos" }
 variable "desired_count" { default = 1 }
